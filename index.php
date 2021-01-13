cd13ddc2ac4c7544fd11ca8022c89df3d67cdb76
Erik Trejo <erik.trejob@gmail.com>
2021-01-13 14:03:50 -0600

    login en una pantalla aparte

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <nav class="my-2 my-md-0 mr-md-3">
             <a href="html/registro.html" class="p-2 text-dark">Registrarme</a>
         </nav>
-        <a class="btn btn-primary text-white" data-toggle="modal" data-target="#loginModal">Iniciar Sesión</a>
+        <a class="btn btn-primary text-white" href="php/login.php">Iniciar Sesión</a>
     </div>
 
     <!-- IMAGEN -->
@@ -25,7 +25,7 @@
 						<h1 class="p-2 m-5 text-primary">Playeras</h1>
 						<h3>Playeras personalizadas</h3>
 						<p class="p-3">Encuentra las playeras de tu elección y escoge el estampado que te guste. </p>
-						<a class="btn btn-primary btn-lg text-white m-3" data-toggle="modal" data-target="#loginModal">
+						<a class="btn btn-primary btn-lg text-white m-3" href="php/login.php">
 							Comprar
 						</a>
 					</div>
@@ -34,49 +34,6 @@
 		</div>
 	</header>
 
-    <!-- modal iniciar sesion-->
-    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content px-4">
-				<div class="modal-header border-bottom-0">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body px-4">
-					<div class="form-title text-center pb-4">
-						<h4>Login</h4>
-					</div>
-					<div class="d-flex flex-column text-center">
-						<form method="post" action="php/login.php">
-							<div class="form-group pb-3">
-								<input type="text" name="usuario" class="form-control" id="usuario" placeholder="Ingresa tu usuario">
-							</div>
-							<div class="form-group pb-3">
-								<input type="password" name="clave" class="form-control" id="password1" placeholder="Ingresa tu contraseña">
-							</div>
-							<!--<div class="alert"><//?php echo isset($alert)? $alert : ''; ?></div>-->
-							<button type="submit" class="btn btn-primary btn-block btn-round">Login</button>
-						</form>
-					</div>
-				</div>
-            	<div class="modal-footer d-flex justify-content-center">
-                    <div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div>
-                	<div class="signup-section">No tengo cuenta, quiero <a href="html/registro.html" class="text-primary"> registrarme</a>.</div>
-            	</div> 
-			</div>	
-        </div>
-    </div>
-
-	<!--<section id="container">
-		<form action="" method="post">
-			<h3>Iniciar Secion</h3>
-			<img src="">
-			<input type="text" name="suario" placeholder="Usuario">
-			<input type="password" name="clave" placeholder="Contraseña">
-			<input type="submit" value="INGRESAR">
-		</form>
-	</section>-->
     <!-- javascript -->
 	<script src="https://code.jquery.com/jquery-latest.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
--- php/login.php
+++ php/login.php
@@ -1,11 +1,10 @@
 <?php  
 
-
-	$alert = '';
+    $alert = '';
 	if (!empty($_POST)) {
         
         if (empty($_POST['usuario']) || empty($_POST['clave'])) {
-			echo $alert="ingrese los campos";
+			$alert = "ingrese su usuario y contraseña.";
 		}else{
             require_once "conexion.php";
             
@@ -30,10 +29,66 @@
 
                 header('location: ../html/'); 
             } else {
-                print_r("Datos incorrectos");
-                $alert = 'El usuario o la clave son incorrectos';
+                $alert = 'El usuario o la contraseña son incorrectos.';
                  
             }
 		}
-	}
+    }
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+</head>
+<body class="bg-light">
+    <!-- header -->
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm"> 
+        <img class="d-block mr-3" src="../img/logo-playeras.jpeg" width="40" height="40">
+        <h5 class="mg-0 mr-md-auto font-weight-bold">Playeras</h5>
+        <a href="../index.php" class="btn btn-primary text-white">Regresar</a>
+    </div>
+    
+    <div class="container">
+        <div class="row justify-content-center pt-5">
+            <div class="col col-lg-6 col-sm-10">
+               
+                <!-- modal iniciar sesion-->
+                <div class="modal-content px-4">
+                    <div class="modal-body px-4">
+                        <div class="form-title text-center pb-4">
+                            <h4>Inicio de sesión</h4>
+                        </div>
+                        <div class="d-flex flex-column text-center">
+                            <form method="post" action="">
+                                <div class="form-group pb-3">
+                                    <input type="text" name="usuario" class="form-control" id="usuario1" placeholder="Ingresa tu usuario" required>
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="clave" class="form-control" id="password1" placeholder="Ingresa tu contraseña" required>
+                                    <div class="alert text-danger">
+                                        <?php echo isset($alert)? $alert : ''; ?>
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-block btn-round">Iniciar sesión</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="modal-footer d-flex justify-content-start">
+                        <div class="signup-section">No tengo cuenta, quiero <a href="../html/registro.html" class="text-primary"> registrarme</a>.</div>
+                    </div>
+                </div>	
+            </div>
+        </div>
+    </div>
+
+    
+
+     <!-- javascript -->
+	<script src="https://code.jquery.com/jquery-latest.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
