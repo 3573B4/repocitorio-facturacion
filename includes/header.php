315b3d01316bb46cea73bc1f43ea153429bb09f8
Erik Trejo <erik.trejob@gmail.com>
2021-01-18 17:56:18 -0600

    Pequeños cambios y nombre en el header

--- includes/header.php
+++ includes/header.php
@@ -15,9 +15,22 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        <ul class="navbar-nav px-3">
+        <!--<ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
-            <a class="nav-link" href="../php/salir.php"">Cerrar sisión</a>
+                <span></span>
             </li>
-        </ul>
+            <li class="nav-item text-nowrap">
+                <a class="nav-link" href="../php/salir.php">Cerrar sisión</a>
+            </li>
+        </ul>-->
+
+        <div class="dropdown">
+            <button class="btn btn-default text-white dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                <?php echo $_SESSION['user']; ?>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                <li><a class="dropdown-item" href="#">Editar perfil</a></li>
+                <li><a class="dropdown-item" href="../php/salir.php">Cerrar sesión</a></li>
+            </ul>
+        </div>
     </header>
