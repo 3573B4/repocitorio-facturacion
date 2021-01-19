9e700bb476f4feb5807230509f34020be50b48a6
ejtbestyk16 <ejtbestyk16@gmail.com>
2021-01-19 01:08:49 -0600

    se agrego La lista de Usuarios

---
+++ html/lista_usuarios.php
@@ -0,0 +1,115 @@
+
+<?php 
+
+    include "../php/conexion.php";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <?php include "../includes/scripts.php";?>
+    <title>Catalogo</title>
+    
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+     <!--Estilos dashboard -->
+     <link rel="stylesheet" type="text/css" href="../css/estilos-admin.css">
+
+</head>
+    <body>
+    
+        <?php include "../includes/header.php";?>
+
+        <div class="container-fluid">
+            <div class="row justify-content-end">
+                <?php include "../includes/nav.php"; ?>
+
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+                <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm"> 
+                    <h2 class="mg-0 mr-md-auto font-weight-bold">Lista de Usuarios</h2>
+                    <a href="crear_usuario.php" class="btn btn-sm btn-outline-secondary">Crear Usuario</a>
+                </div>
+                
+                <div class="table-responsive">
+                    <table class="table table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th>Contador</th>
+                                <th>Nombre Completo</th>
+                                <th>Correo Electronico</th>
+                                <th>Usuarios</th>
+                                <th>Rol</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+
+                    <?php
+                        
+                        $query = mysqli_query($connection, "SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol");
+                        $result = mysqli_num_rows($query);
+
+                        if ($result > 0) {
+                            while ($data = mysqli_fetch_array($query)) {
+                    ?>
+                                    
+                            <tr>
+                                <td><?php echo $data['idusuario']; ?></td>
+                                <td><?php echo $data['nombre']; ?></td>
+                                <td><?php echo $data['correo']; ?></td>
+                                <td><?php echo $data['usuario']; ?></td>
+                                <td><?php echo $data['rol']; ?></td>
+                                <td>
+
+                                    <a class="btn btn-primary text-white" type="botton" href="#">Editar</a>
+                                    <a class="btn btn-primary text-white" type="botton" href="#">Borrar</a>
+                                
+                                </td>
+                            </tr>
+                    <?php
+
+                            }
+                        }
+                        
+                    ?>
+
+                            
+                            
+                        </tbody>
+                    </table>
+                </div>
+                    
+                </main>
+            </div>
+        </div>
+        <?php include "../includes/footer.php"; //para el pie de pagina ?>
+        <!--<script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>-->
+        <!--<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>-->
+        
+    </body>
+</html>
